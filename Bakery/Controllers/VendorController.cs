43fb79968ff00f566f1d1838b0cba375421e41cf
rochellev <rochellevroberts@utexas.edu>
2019-08-02 12:43:05 -0700

    added vendor details page

--- Bakery/Controllers/VendorController.cs
+++ Bakery/Controllers/VendorController.cs
@@ -27,5 +27,16 @@ namespace Bakery.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = selectedVendor.Orders;
+            model.Add("vendor", selectedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
+        }
+
     }
 }
