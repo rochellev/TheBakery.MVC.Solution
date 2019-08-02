002597fd396c3a113ec95ab247bea936d485250c
rochellev <rochellevroberts@utexas.edu>
2019-08-02 11:05:22 -0700

    added vendor tests

--- Bakery.Tests/ModelTests/VendorTest.cs
+++ Bakery.Tests/ModelTests/VendorTest.cs
@@ -11,14 +11,21 @@ namespace Bakery.Test
         [TestMethod]
         public void ConstructorTest_True()
         {
-            Vendor vendor1 = new Vendor("pippa's place");
+            Vendor vendor1 = new Vendor("pippa's place", "bakery");
             Assert.AreEqual(typeof(Vendor), vendor1.GetType());
         }
 
         [TestMethod]
         public void GetAll_test()
         {
-            
+        Vendor.ClearAll();
+         Vendor vendor1 = new Vendor("pippa's place", "bakery");
+         Vendor vendor2 = new Vendor("pippa's second place", "bakery");
+         List<Vendor> testList = new List<Vendor>{ vendor1, vendor2};
+
+         List<Vendor> resultList = Vendor.GetAll();
+
+         CollectionAssert.AreEqual(testList, resultList);
 
         }
 
